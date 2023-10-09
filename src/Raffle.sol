bfb198a736e71c8367a4117d2254e451c9e2b848
hhhfriends19 <84479916@qq.com>
2023-10-09 21:45:23 +0800

    update Raffle.sol

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,5 +1,50 @@
+// Layout contract elements in the following order:
+
+// Pragma statements
+
+// Import statements
+
+// Interfaces
+
+// Libraries
+
+// Contracts
+
+// Inside each contract, library or interface, use the following order:
+
+// Type declarations
+
+// State variables
+
+// Events
+
+// Modifiers
+
+// Functions
+
 // SPDX-License-Identifier:MIT
 
 pragma solidity ^0.8.18;
 
-contract Raffle {}
+/**
+ * @title A sample Raffle Contract
+ * @author Mavis
+ * @notice This contract is for creating a sample raffle
+ * @dev Implements Chainlink VRFv2
+ */
+contract Raffle {
+    uint256 private immutable i_entranceFee;
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public {}
+
+    function pickWinner() public {}
+
+    /** Getter Function */
+    function getEntranceFee() external view returns (uint256) {
+        return i_entranceFee;
+    }
+}
