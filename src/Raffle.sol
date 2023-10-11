e0e54b2f6eebbf4f106a20f8d19ead7e125750d0
hhhfriends19 <84479916@qq.com>
2023-10-11 17:10:40 +0800

    update Raffle.sol with function enterRaffle

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -33,15 +33,48 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
-    uint256 private immutable i_entranceFee;
+    error Raffle__NotEnoughEthSent();
 
-    constructor(uint256 entranceFee) {
+    uint256 private immutable i_entranceFee;
+    // @dev Duration of the lottery in seconds
+    uint256 private immutable i_interval;
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    /** Events */
+    event EnteredRaffle(address indexed player);
+
+    // What data structure should we use? How to keep track of all the players?
+
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public {}
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enouth ETH sent!");
+        if (msg.value <= i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
+        s_players.push(payable(msg.sender));
+        // event 的作用
+        // 1. Makes migration easier 使合约迁移更方便
+        // 2. Makes front end "indexing" easier 方便前后端进行索引
+        emit EnteredRaffle(msg.sender);
+    }
 
-    function pickWinner() public {}
+    // 1. Get a random number
+    // 2. Use the random number to pick a player
+    // 3. Be automatically called
+    function pickWinner() external {
+        // check to see if enouth time has passed
+        // 1000 - 500 = 500. 600 seconds
+        // 1200 - 500 = 700. 600 seconds
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
+    }
 
     /** Getter Function */
     function getEntranceFee() external view returns (uint256) {
