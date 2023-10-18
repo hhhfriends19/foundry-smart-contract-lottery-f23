fd5c39fb1900c6f2c3ad981ff08cb516bc5c7901
hhhfriends19 <84479916@qq.com>
2023-10-18 17:47:02 +0800

    update HelperConfig.s.sol - add struct constructor getSepoliaEthConfig getOrCreateAnvilEthConfig

---
+++ src/HelperConfig.s.sol
@@ -0,0 +1,45 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig is Script {
+    struct NetworkConfig {
+        uint256 entranceFee;
+        uint256 interval;
+        address vrfCoordinator;
+        bytes32 gasLane;
+        uint64 subscriptionId;
+        uint32 callbackGasLimit;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            // 11155111 是 sepolia 的 chainid
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
+        return
+            NetworkConfig({
+                entranceFee: 0.01 ether,
+                interval: 30,
+                vrfCoordinator: 0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625,
+                gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
+                subscriptionId: 6054, //之前创建的 subscription
+                callbackGasLimit: 500000 // 500,000 gas
+            });
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.vrfCoordinator != address(0)) {
+            return activeNetworkConfig;
+        }
+    }
+}
