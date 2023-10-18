4bf126136ad3ae0a18bc33c9ca0b71f3aa8130e0
hhhfriends19 <84479916@qq.com>
2023-10-18 15:40:40 +0800

    update DeployRaffle.s.sol - add run function

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -3,5 +3,8 @@
 pragma solidity ^0.8.18;
 
 import {Script} from "../forge-std/Script.sol";
+import {Raffle} from "../src/Raffle.sol";
 
-contract DeployRaffle is Script {}
+contract DeployRaffle is Script {
+    function run() external returns (Raffle) {}
+}
