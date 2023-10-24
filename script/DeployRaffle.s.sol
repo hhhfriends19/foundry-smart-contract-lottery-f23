89ba818fd9be89513eaa581e3c82560902c11a59
hhhfriends19 <84479916@qq.com>
2023-10-24 11:03:33 +0800

    add fund subscription

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.8.18;
 import {Script} from "../forge-std/Script.sol";
 import {Raffle} from "../src/Raffle.sol";
 import {HelperConfig} from "../script/HelperConfig.s.sol";
-import {CreateSubscription} from "./Interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "./Interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() external returns (Raffle, HelperConfig) {
@@ -28,9 +28,13 @@ contract DeployRaffle is Script {
             );
 
             // Fund it!
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                vrfCoordinator,
+                subscriptionId,
+                link
+            );
         }
-
-        if (subscriptionId == 0) {}
 
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -43,6 +47,13 @@ contract DeployRaffle is Script {
             link
         );
         vm.stopBroadcast();
+
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            vrfCoordinator,
+            subscriptionId
+        );
         return (raffle, helperConfig);
     }
 }
